b45d1c2ef2dd31456a5d31305a97504fe9bed70f
Saint of Grey <42332565+Saint-of-Grey@users.noreply.github.com>
2018-11-24 16:15:29 -0800

    I'VE FINALLY DONE IT
    After much misery, I've finally given droids a mechanoid health card! Thanks much to menhi for his help. Also gave them some organs, to tip the scales of balance a bit. Now that their as tanky as humans again, might as well give them some more critical hit weakness.

---
+++ Source/CapacityTranspiler.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using Harmony;
+using RimWorld;
+using Verse;
+using System.Reflection;
+using System.Reflection.Emit;
+using RoboticEqaulity;
+
+//would you mind writing a small transpiler for him?
+//sadly not near an IDE atm
+
+//callvirt Verse.RaceProperties::get_IsFlesh()
+
+//replaced with
+
+//call YourClass::IsFleshFix(RaceProperties)
+
+
+//then place his code inside of
+
+//public static bool IsFleshFix(RaceProperties raceProps)
+
+
+//then he can place the mod extension on his custom flesh type def, and be done with it
+//transpiler doesn't need any other conditions, just replace that call with another one
+
+namespace TranspilerForPawnCapacityDefReporting
+{
+	[StaticConstructorOnStartup]
+	public static class Class1
+	{
+		static Class1()
+		{
+			HarmonyInstance harmony = HarmonyInstance.Create("mehni.rimworld.TranspilerForPawnCapacityDefReporting");
+			harmony.Patch(AccessTools.Method(typeof(HealthCardUtility), "DrawOverviewTab"),
+				transpiler: new HarmonyMethod(typeof(Class1), nameof(TranspilerForPawnCapacityDefReporting)));
+		}
+
+		private static IEnumerable<CodeInstruction> TranspilerForPawnCapacityDefReporting(IEnumerable<CodeInstruction> instructions)
+		{
+			foreach (CodeInstruction codeInstruction in instructions)
+			{
+				if (codeInstruction.opcode == OpCodes.Callvirt && codeInstruction.operand == (AccessTools.Property(typeof(RaceProperties), nameof(RaceProperties.IsFlesh)).GetGetMethod()))
+				{
+					codeInstruction.opcode = OpCodes.Call;
+					codeInstruction.operand = AccessTools.Method(typeof(Class1), nameof(IsFleshFix));
+				}
+				//Log.Message("It did a thing");
+				yield return codeInstruction;
+			}
+		}
+
+		public static bool IsFleshFix(RaceProperties raceProps)
+		{
+			if (raceProps.FleshType.HasModExtension<MechanicalCheck>())
+			{
+				return (raceProps.FleshType.GetModExtension<MechanicalCheck>()?.isMechanical ?? false);
+			}
+			else
+			{
+				return raceProps.FleshType != FleshTypeDefOf.Mechanoid;
+			}
+		}
+	}
+}
---
+++ Source/isMechanical.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Harmony;
+using RimWorld;
+using Verse;
+using TranspilerForPawnCapacityDefReporting;
+
+namespace RoboticEqaulity
+{
+	public class MechanicalCheck : DefModExtension
+	{
+		public bool isMechanical = false;
+
+		public MechanicalCheck()
+		{
+		}
+	}
+
+	[StaticConstructorOnStartup]
+    public static class RobotPatch  
+    {
+        static RobotPatch()
+        {
+            HarmonyInstance harmony = HarmonyInstance.Create(id: "Grey.Robotic.Equality");
+            
+            harmony.Patch(original: AccessTools.Method(type: typeof(PawnCapacityDef), name: nameof(PawnCapacityDef.GetLabelFor), parameters: new Type[] { typeof(Pawn) }), 
+                prefix: null, postfix: new HarmonyMethod(type: typeof(RobotPatch), name: nameof(MakeRobotic)));
+			harmony.Patch(original: AccessTools.Method(type: typeof(PawnCapacityDef), name: nameof(PawnCapacityDef.GetLabelFor), parameters: new Type[] { typeof(bool), typeof(bool) }), 
+				prefix: null, postfix: new HarmonyMethod(type: typeof(RobotPatch), name: nameof(ISaidMakeRoboticDammit)));
+        }
+
+        private static void MakeRobotic(PawnCapacityDef __instance, Pawn pawn, ref string __result)
+        {
+			if (pawn.RaceProps.FleshType.GetModExtension<MechanicalCheck>()?.isMechanical ?? false)
+			{
+				if (!__instance.labelMechanoids.NullOrEmpty())
+				{
+					__result = __instance.labelMechanoids;
+				}
+			}
+		}
+		private static void ISaidMakeRoboticDammit(PawnCapacityDef __instance, bool isFlesh , bool isHumanlike, ref string __result)
+		{
+			if (isFlesh && isHumanlike)
+			{
+				if (!__instance.labelMechanoids.NullOrEmpty())
+				{
+					__result = __instance.labelMechanoids;
+				}
+			}
+		}
+	}
+}
